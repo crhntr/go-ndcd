f08c8220d056792e79d37a3a63405eabd15f5656
crhntr <crhntr.com@gmail.com>
2017-04-11 22:49:32 -0700

    removed Download code
    each client should write their own doanload code
    an example can be found in NDCD main test function

--- NDCD_test.go
+++ NDCD_test.go
@@ -1,10 +1,16 @@
 package ndcd_test
 
 import (
+	"archive/zip"
 	"flag"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"testing"
 )
+
+const NDC_ZIP_URL = "http://www.accessdata.fda.gov/cder/ndctext.zip"
 
 func TestMain(m *testing.M) {
 	if _, err := os.Stat("testdata"); err != nil {
@@ -16,6 +22,69 @@ func TestMain(m *testing.M) {
 			panic(err)
 		}
 	}
+
+	_, packageMissing := os.Stat("testdata/package.txt")
+	_, productMissing := os.Stat("testdata/product.txt")
+
+	if packageMissing != nil || productMissing != nil {
+		var zipFile *os.File
+
+		if _, zipMissing := os.Stat("testdata/package.txt"); zipMissing != nil {
+			getResponse, err := http.Get(NDC_ZIP_URL)
+			if err != nil {
+				panic(err)
+			}
+			defer getResponse.Body.Close()
+
+			zipFile, err = os.Create("testdata/ndctext.zip")
+			if err != nil {
+				log.Panic(err)
+			}
+			defer zipFile.Close()
+
+			io.Copy(zipFile, getResponse.Body)
+		}
+
+		zipReader, err := zip.OpenReader("testdata/ndctext.zip")
+		if err != nil {
+			log.Panic(err)
+		}
+		defer zipReader.Close()
+
+		for _, f := range zipReader.File {
+			switch f.Name {
+			case "product.txt":
+				if productMissing == nil {
+					continue
+				}
+				os.Remove("testdata/product.txt")
+				file, err := os.Create("testdata/product.txt")
+				if err != nil {
+					log.Panic(err)
+				}
+				productFile, err := f.Open()
+				if err != nil {
+					log.Panic(err)
+				}
+				io.Copy(file, productFile)
+			case "package.txt":
+				if packageMissing == nil {
+					continue
+				}
+				os.Remove("testdata/package.txt")
+				file, err := os.Create("testdata/package.txt")
+				if err != nil {
+					log.Panic(err)
+				}
+				packageFile, err := f.Open()
+				if err != nil {
+					log.Panic(err)
+				}
+				io.Copy(file, packageFile)
+			}
+		}
+	}
+
 	flag.Parse()
 	os.Exit(m.Run())
 }
